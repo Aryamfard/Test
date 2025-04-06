afc3b71ed2ae12a8d1560834c1886eddd1a8aade
Arya Mohamadifard <arya.originalmail@gmail.com>
2025-04-07 00:28:58 +0330

    add minus and getch

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main() {
     // Declare variables
-    int num1, num2, sum;
+    int num1, num2, sum, minus;
 
     // Ask user for input
     cout << "Enter first number: ";
@@ -14,9 +15,12 @@ int main() {
 
     // Calculate the sum
     sum = num1 + num2;
+    minus = num1 - num2;
 
     // Display the result
     cout << "The sum is: " << sum << endl;
-
+    cout << "The minus is: " << minus << endl;
+    
+    getch();
     return 0;
 }
